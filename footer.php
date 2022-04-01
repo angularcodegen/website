823352652d20c4b7d309e190072031b1abac1ea0
Adrian Bury <adrian.jakub.bury@gmail.com>
2022-04-01 11:49:13 +0200

    Add footer

--- footer.php
+++ footer.php
@@ -1,3 +1,39 @@
+<style>
+    @media (max-width: 600px) {
+        #site-footer > .container {
+            display: flex;
+            flex-direction: column;
+        }
+    }
+    #site-footer {
+        text-align: center;
+        padding: 15px;
+        background-color: rgb(47 51 55 / 80%);
+        font-size: 0.8rem;
+        margin-top: 5rem;
+    }
+
+    #site-footer > .container {
+        display: flex;
+        justify-content: space-between;
+        gap: 1rem;
+    }
+</style>
+
+<footer id="site-footer">
+    <div class="container">
+        <span>
+            <?= __('Strona dumnie wspierana przez', 'cg') ?>
+            <a href="https://wordpress.org">WordPress</a>
+        </span>
+        <span>
+            <?= __('Motyw własny dostępnt na stronie', 'cg') ?>
+            <a href="https://github.com/angularcodegen/website">GitHub</a>
+        </span>
+    </div>
+</footer>
+
+
 <?php
 wp_footer();
 
--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>"/>
     <meta name="viewport" content="width=device-width"/>
-    <title><?php wp_title('|', true, 'right'); ?></title>
+    <title><?php wp_title('|', true, 'right'); ?><?= get_option('blogname'); ?></title>
     <link rel="profile" href="https://gmpg.org/xfn/11"/>
     <link rel="pingback" href="<?php echo esc_url(get_bloginfo('pingback_url')); ?>">
     <link rel="stylesheet" href="<?php echo get_template_directory_uri() . '/style.css'; ?>">
