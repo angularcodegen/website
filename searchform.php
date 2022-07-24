047b4477c5fe9880ddca2fc02290c9e8f4212939
Adrian Bury <adrian.jakub.bury@gmail.com>
2022-07-24 20:09:34 +0200

    Set autofocus for search and 404

--- searchform.php
+++ searchform.php
@@ -3,6 +3,7 @@
 $action = esc_url(home_url('/'));
 $placeholder = esc_attr_x('Search &hellip;', 'placeholder');
 $query = get_search_query();
+$autofocus = is_404() || is_search();
 ?>
 
 <style>
@@ -30,8 +31,15 @@ $query = get_search_query();
 
 <form role="search" method="get" class="search-form" action="<?= $action ?>">
     <label>
-        <input type="search" class="search-field" placeholder="<?= $placeholder ?>" value="<?= $query ?>" name="s"
-               autocomplete="off"/>
+        <input
+            <?= $autofocus ? 'autofocus' : '' ?>
+                type="search"
+                class="search-field"
+                placeholder="<?= $placeholder ?>"
+                value="<?= $query ?>"
+                name="s"
+                autocomplete="off"
+        />
     </label>
     <button type="submit" class="search-submit">
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search"
