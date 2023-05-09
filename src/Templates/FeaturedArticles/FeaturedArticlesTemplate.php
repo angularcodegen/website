ecf2f30c774f37d59acb00b50771023391d172df
Adrian Bury <adrian.jakub.bury@gmail.com>
2023-05-09 22:48:43 +0200

    Display feature articles from diffrent categories

--- src/Templates/FeaturedArticles/FeaturedArticlesTemplate.php
+++ src/Templates/FeaturedArticles/FeaturedArticlesTemplate.php
@@ -2,12 +2,50 @@
 
 namespace CG\Templates\FeaturedArticles;
 
+use WP_Query;
+
 class FeaturedArticlesTemplate
 {
+
+    public WP_Query $feature_article_query;
+    public WP_Query|null $extra_feature_article_query;
+    private int $current_post_id;
+    private array $current_post_categories;
+    private int $posts_per_page;
 
     public function __construct()
     {
         add_action('get_footer', array($this, 'register_styles'));
+
+        $this->current_post_id = get_the_ID();
+        $this->current_post_categories = wp_get_post_categories($this->current_post_id);
+        $this->posts_per_page = 6;
+
+        $this->feature_article_query = $this->get_feature_article_query();
+        if ($this->feature_article_query->post_count < $this->posts_per_page) {
+            $this->extra_feature_article_query = $this->get_extra_feature_article_query();
+        } else {
+            $this->extra_feature_article_query = null;
+        }
+    }
+
+    private function get_feature_article_query(): WP_Query
+    {
+        return new WP_Query(array(
+            'posts_per_page' => $this->posts_per_page,
+            'post_type' => 'post',
+            'post__not_in' => array($this->current_post_id),
+            'category__in' => $this->current_post_categories
+        ));
+    }
+
+    private function get_extra_feature_article_query(): WP_Query
+    {
+        return new WP_Query(array(
+            'posts_per_page' => $this->posts_per_page - $this->feature_article_query->post_count,
+            'post_type' => 'post',
+            'category__not_in' => $this->current_post_categories
+        ));
     }
 
     public function render(): void
@@ -23,6 +61,9 @@ class FeaturedArticlesTemplate
         );
     }
 
-
+    public function get_article_template(): string
+    {
+        return require 'FeatureArticleTemplate.phtml';
+    }
 
 }
