d213f89d1885ff481de8b893a61f0bcaf448cbe7
Adrian Bury <adrian.jakub.bury@gmail.com>
2023-09-27 20:52:15 +0000

    Add google ad sense integration

--- src/Integrations/ThemeIntegrations.php
+++ src/Integrations/ThemeIntegrations.php
@@ -4,12 +4,14 @@ namespace CG\Integrations;
 
 use CG\Integrations\Acf\AcfIntegration;
 use CG\Integrations\GoogleTagManager\GoogleTagManagerIntegration;
+use CG\Integrations\GoogleAdSense\GoogleAdSense;
 
 class ThemeIntegrations
 {
     public static function turn_on_all():void {
         AcfIntegration::turn_on();
         GoogleTagManagerIntegration::turn_on();
+        GoogleAdSense::turn_on();
     }
 
 }
