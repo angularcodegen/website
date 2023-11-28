93971272e1f94e149df5ddda480aecb676e31c10
Adrian Bury <adrian.jakub.bury@gmail.com>
2023-11-28 20:40:35 +0100

    Add styles for image block

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@ use CG\Plugins\RepositoryUpdateWebHook\RepositoryUpdateWebHook;
 use CG\Seo\Seo;
 use CG\ThemeOptions;
 
-require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__.'/vendor/autoload.php';
 
 Seo::turn_on();
 ContactForm::init();
@@ -19,53 +19,52 @@ RepositoryUpdateWebHook::init();
 
 add_action('all_admin_notices', 'check_required_plugins');
 
-function check_required_plugins()
-{
+function check_required_plugins() {
     $required_plugins = [
         [
             'name' => 'ACF',
             'url' => 'https://www.advancedcustomfields.com/',
-            'slugs' => ['advanced-custom-fields/acf.php', 'advanced-custom-fields-pro/acf.php']
-        ]
+            'slugs' => ['advanced-custom-fields/acf.php', 'advanced-custom-fields-pro/acf.php'],
+        ],
     ];
 
-    foreach ($required_plugins as $plugin) {
+    foreach($required_plugins as $plugin) {
         $is_active = false;
-        foreach ($plugin['slugs'] as $slug) {
-            if (is_plugin_active($slug)) {
+        foreach($plugin['slugs'] as $slug) {
+            if(is_plugin_active($slug)) {
                 $is_active = true;
                 break;
             }
         }
 
-        if (!$is_active) {
+        if(!$is_active) {
             show_missing_plugin_error($plugin['name'], $plugin['url']);
         }
     }
 }
 
-function show_missing_plugin_error($name, $url)
-{
+function show_missing_plugin_error($name, $url) {
     ?>
     <div class="notice notice-error">
-        <p>The <?= $name ?> plugin is required for this theme. Please <a href="<?= $url ?>">install it here</a>.</p>
+        <p>The
+            <?= $name ?> plugin is required for this theme. Please <a href="<?= $url ?>">install it here</a>.
+        </p>
     </div>
     <?php
 }
 
 add_image_size('post_tile', 320, 180, array('center', 'center'));
 
-function get_thumbnail_id_from_tree(): int
-{
-    if (has_post_thumbnail()) {
+function get_thumbnail_id_from_tree(): int {
+    if(has_post_thumbnail()) {
         return get_post_thumbnail_id();
     }
 
     $terms = get_the_category();
 
-    foreach ($terms as $term) {
+    foreach($terms as $term) {
         $field = get_field('44008F46AA4D4368B1634E38B19A873F', $term);
-        if ($field !== false && $field !== null) {
+        if($field !== false && $field !== null) {
             return $field['id'];
         }
     }
@@ -74,8 +73,7 @@ function get_thumbnail_id_from_tree(): int
     return $default['id'];
 }
 
-function mytheme_register_nav_menu(): void
-{
+function mytheme_register_nav_menu(): void {
     register_nav_menus(array(
         'main-menu' => __('Main menu', 'cg'),
     ));
@@ -84,21 +82,20 @@ function mytheme_register_nav_menu(): void
 add_action('after_setup_theme', 'mytheme_register_nav_menu', 0);
 
 add_theme_support('html5', array(
-        'search-form',
-        'comment-list',
-        'comment-form',
-        'gallery',
-        'caption',
-    )
+    'search-form',
+    'comment-list',
+    'comment-form',
+    'gallery',
+    'caption',
+)
 );
 
 add_theme_support('post-thumbnails');
 
-function no_self_ping(&$links): void
-{
+function no_self_ping(&$links): void {
     $home = get_option('home');
-    foreach ($links as $l => $link) {
-        if (str_contains($link, $home)) {
+    foreach($links as $l => $link) {
+        if(str_contains($link, $home)) {
             unset($links[$l]);
         }
     }
@@ -106,8 +103,7 @@ function no_self_ping(&$links): void
 
 add_action('pre_ping', 'no_self_ping');
 
-function remove_wp_block_library_css(): void
-{
+function remove_wp_block_library_css(): void {
     wp_dequeue_style('wp-block-library');
     wp_dequeue_style('wp-block-library-theme');
     wp_dequeue_style('wc-blocks-style');
@@ -123,16 +119,15 @@ remove_action('wp_print_styles', 'print_emoji_styles');
 remove_action('admin_print_scripts', 'print_emoji_detection_script');
 remove_action('admin_print_styles', 'print_emoji_styles');
 
-function redirect_to_full_url_if_needed(): void
-{
-    if (get_the_ID() === false || is_single() === false) {
+function redirect_to_full_url_if_needed(): void {
+    if(get_the_ID() === false || is_single() === false) {
         return;
     }
 
-    $current_url = get_home_url() . $_SERVER['REQUEST_URI'];
+    $current_url = get_home_url().$_SERVER['REQUEST_URI'];
     $full_url = get_permalink();
 
-    if (str_contains($current_url, $full_url)) {
+    if(str_contains($current_url, $full_url)) {
         return;
     }
 
