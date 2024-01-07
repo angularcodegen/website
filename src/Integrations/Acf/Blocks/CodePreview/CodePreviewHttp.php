db60347e41e1901624514369029645343d8f063a
Adrian Bury <adrian.jakub.bury@gmail.com>
2024-01-07 15:27:47 +0100

    Add better error handler

--- src/Integrations/Acf/Blocks/CodePreview/CodePreviewHttp.php
+++ src/Integrations/Acf/Blocks/CodePreview/CodePreviewHttp.php
@@ -6,6 +6,8 @@ use CG\Core\AppLogger;
 
 class CodePreviewHttp
 {
+    public \Monolog\Logger $logger;
+
     public static function format_and_highlight($language, $code): string
     {
         if (empty($language) || empty($code)) {
@@ -29,20 +31,20 @@ class CodePreviewHttp
         $timeConsumed = round(microtime(true) - $curTime, 3) * 1000;
 
         if ($timeConsumed > 2_000) {
-            AppLogger::getLogger()->warning("code has been format and highlighted in more than 2s", [
+            self::$logger->warning("code has been format and highlighted in more than 2s", [
                 "requestTime" => $timeConsumed,
                 "unit" => "ms"
             ]);
         } else {
-            AppLogger::getLogger()->info("Code has been formatted and highlighted", [
+            self::$logger->info("Code has been formatted and highlighted", [
                 "requestTime" => $timeConsumed,
                 "unit" => "ms"
 
             ]);
         }
 
-        if (is_wp_error($response)) {
-            AppLogger::getLogger()->error('Code failed to format and highlight', [
+        if (is_wp_error($response) || wp_remote_retrieve_response_code($response) !== 200) {
+            self::$logger->error('Code failed to format and highlight', [
                 "code" => $code,
                 "wp_error" => $response
             ]);
@@ -76,14 +78,14 @@ class CodePreviewHttp
         );
         $timeConsumed = round(microtime(true) - $curTime, 3) * 1000;
 
-        AppLogger::getLogger()->info("Code has been highlighted", [
+        self::$logger->info("Code has been highlighted", [
             "requestTime" => $timeConsumed,
             "unit" => "ms"
 
         ]);
 
-        if (is_wp_error($response)) {
-            AppLogger::getLogger()->error('Code failed to highlight', [
+        if (is_wp_error($response) || wp_remote_retrieve_response_code($response) !== 200) {
+            self::$logger->error('Code failed to highlight', [
                 "code" => $code,
                 "wp_error" => $response
             ]);
@@ -120,16 +122,23 @@ class CodePreviewHttp
         $timeConsumed = round(microtime(true) - $curTime, 3) * 1000;
 
 
-        AppLogger::getLogger()->info("Code has been formatted", [
+        self::$logger->info("Code has been formatted", [
             "requestTime" => $timeConsumed,
             "unit" => "ms"
 
         ]);
 
-        if (is_wp_error($response)) {
+        if (is_wp_error($response) || wp_remote_retrieve_response_code($response) !== 200) {
+            self::$logger->error('Code failed to highlight', [
+                "code" => $code,
+                "wp_error" => $response
+            ]);
+
             return $code;
         }
 
         return $response['body'];
     }
 }
+
+CodePreviewHttp::$logger = AppLogger::getLogger()->withName('CodePreviewHttp');
